740c9b4292c94ff239ae7f2b88472634928be8b0
jcorreaw <juninhocorrea2014@gmail.com>
2023-08-30 15:46:34 -0300

    Comparable e Comparator

---
+++ src/estudos/comparableXcomparator/Livro.java
@@ -0,0 +1,78 @@
+package estudos.comparableXcomparator;
+
+import java.util.Comparator;
+
+public class Livro implements Comparable<Livro> {
+    private String titulo;
+    private String autor;
+    private int ano;
+
+    public Livro(String titulo, String autor, int ano) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.ano = ano;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public int getAno() {
+        return ano;
+    }
+
+    public void setAno(int ano) {
+        this.ano = ano;
+    }
+
+    @Override
+    public int compareTo(Livro l) {
+        return titulo.compareTo(l.titulo);
+    }
+}
+class CompararAutor implements Comparator<Livro> {
+    @Override
+    public int compare(Livro l1, Livro l2) {
+        return l1.getAutor().compareTo(l2.getAutor());
+    }
+}
+
+class CompararAno implements Comparator<Livro> {
+    @Override
+    public int compare(Livro l1, Livro l2) {
+        return Integer.compare(l1.getAno(), l2.getAno());
+		/*
+		if (l1.getAno() < l2.getAno())
+			return -1;
+		if (l1.getAno() > l2.getAno())
+			return 1;
+		else
+			return 0;
+		 */
+    }
+}
+
+class CompararAnoAutorTitulo implements Comparator<Livro> {
+    @Override
+    public int compare(Livro l1, Livro l2) {
+        int ano = Integer.compare(l1.getAno(), l2.getAno());
+        if (ano != 0)
+            return ano;
+        int autor = l1.getAutor().compareTo(l2.getAutor());
+        if (autor != 0)
+            return autor;
+        return l1.getTitulo().compareTo(l2.getTitulo());
+    }
+}
