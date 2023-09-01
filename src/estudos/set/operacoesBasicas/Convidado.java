02450785faf9af5bc4344ce9416b75d1e513005f
jcorreaw <juninhocorrea2014@gmail.com>
2023-09-01 11:17:16 -0300

    equals

--- src/estudos/set/operacoesBasicas/Convidado.java
+++ src/estudos/set/operacoesBasicas/Convidado.java
@@ -1,5 +1,7 @@
 package estudos.set.operacoesBasicas;
 
+import java.util.Objects;
+
 public class Convidado {
     private String nome;
     private int codigoConvite;
@@ -23,5 +25,18 @@ public class Convidado {
                 "nome='" + nome + '\'' +
                 ", codigoConvite=" + codigoConvite +
                 '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Convidado convidado = (Convidado) o;
+        return codigoConvite == convidado.codigoConvite;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(codigoConvite);
     }
 }
